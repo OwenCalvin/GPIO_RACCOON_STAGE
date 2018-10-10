c663d5c500fca63d108986a364a83c1ac9c00d62
Owen Gombas <owen.gombas@gmail.com>
2018-10-10 10:59:18 +0200

    Split file

---
+++ front/app.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector('#btn')
+let on = false
+const server = axios.create({
+  baseURL: 'http://localhost:4500/'
+})
+
+function resetButton () {
+  btn.disabled = true
+  btn.innerHTML = '...'
+}
+
+function updateButton (onOff) {
+  on = onOff
+  btn.disabled = false
+  if (on) {
+    btn.innerHTML = 'Eteindre'
+  } else {
+    btn.innerHTML = 'Allumer'
+  }
+  // btn.innerHTML = on ? 'Eteindre' : 'Allumer'
+}
+
+server.get('on').then((res) => {
+  updateButton(res.data.on)
+})
+
+btn.addEventListener('click', () => {
+  resetButton()
+  server.post('on').then(res => {
+    updateButton(res.data.on)
+  })
+})
